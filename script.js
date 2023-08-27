0929be7c7fd7ad1910e9269b1f1e204b484a4a82
Ajay Adikari <94204251+ajayadikari@users.noreply.github.com>
2023-08-27 11:16:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,27 +6,29 @@ const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
+	
 ];
 
-Promise.all(images)
-.then((values)=>{
-	// values.foreach(img =>{
-	// 	const imgEl = document.createElement('img');
-	// 	imgEl.src = 'img';
-	// 	output.append(imgEl);
 
-		
-	// 	const h1 = document.createElement('h1');
-	// 	h1.innerText('working')
-	// 	output.append(h1)
-		
-	// })
-
+btn.addEventListener('click', ()=>{
 	const h1 = document.createElement('h1');
-	h1.innerText('working')
+	h1.innerText = 'loading please wait...'
 	output.append(h1)
-
+	fillWithImg();
 })
+
+function fillWithImg(){
+	Promise.all(images)
+	.then((arr)=>{
+		output.innerText = ''
+		arr.forEach((img)=>{
+			const imgEl = document.createElement('img');
+			imgEl.src = img.url;
+			output.append(imgEl);
+		})
+	})
+}
+
 
 
 
