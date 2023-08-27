4214ee00b1b95bab307026d2e10e88fa623d9913
Ajay Adikari <94204251+ajayadikari@users.noreply.github.com>
2023-08-27 10:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+Promise.all(images)
+.then((values)=>{
+	// values.foreach(img =>{
+	// 	const imgEl = document.createElement('img');
+	// 	imgEl.src = 'img';
+	// 	output.append(imgEl);
+
+		
+	// 	const h1 = document.createElement('h1');
+	// 	h1.innerText('working')
+	// 	output.append(h1)
+		
+	// })
+
+	const h1 = document.createElement('h1');
+	h1.innerText('working')
+	output.append(h1)
+
+})
+
+
+
+
+
